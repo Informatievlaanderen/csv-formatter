970534a272994ca51ff6652bd8d2d9e040020095
Arne Dumarey <dumarey.arne@gmail.com>
2025-04-01 14:12:50 +0200

    refactor: use renovate and nuget + update pipeline
    BREAKING CHANGE: update to dotnet 9

--- src/Be.Vlaanderen.Basisregisters.AspNetCore.Mvc.Formatters.Csv/CsvOutputFormatter.cs
+++ src/Be.Vlaanderen.Basisregisters.AspNetCore.Mvc.Formatters.Csv/CsvOutputFormatter.cs
@@ -31,10 +31,9 @@ namespace Be.Vlaanderen.Basisregisters.AspNetCore.Mvc.Formatters.Csv
             //SupportedEncodings.Add(Encoding.GetEncoding("utf-8"));
         }
 
-        protected override bool CanWriteType(Type type)
+        protected override bool CanWriteType(Type? type)
         {
-            if (type == null)
-                throw new ArgumentNullException(nameof(type));
+            ArgumentNullException.ThrowIfNull(type);
 
             return IsTypeOfIEnumerable(type);
         }
@@ -43,37 +42,39 @@ namespace Be.Vlaanderen.Basisregisters.AspNetCore.Mvc.Formatters.Csv
 
         public override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context)
         {
+            ArgumentNullException.ThrowIfNull(context);
+            ArgumentNullException.ThrowIfNull(context.Object);
+
             var response = context.HttpContext.Response;
 
-            Type type = context.Object.GetType();
+            var type = context.Object.GetType();
             var itemType = type.GetGenericArguments().Length > 0 ? type.GetGenericArguments()[0] : type.GetElementType();
+
+            ArgumentNullException.ThrowIfNull(itemType);
 
             var stringWriter = new StringWriter();
 
             if (_options.UseSingleLineHeaderInCsv)
-                stringWriter.WriteLine(string.Join<string>(_options.CsvDelimiter, itemType.GetProperties().Select(x => x.Name)));
+                await stringWriter.WriteLineAsync(string.Join<string>(_options.CsvDelimiter, itemType.GetProperties().Select(x => x.Name)));
 
             foreach (var obj in (IEnumerable<object>)context.Object)
             {
-                var vals = obj.GetType().GetProperties().Select(pi => new { Value = pi.GetValue(obj, null) });
+                var vals = obj.GetType().GetProperties().Select(pi => pi.GetValue(obj, null));
 
                 var valueLine = string.Empty;
 
                 foreach (var val in vals)
                 {
-                    if (val.Value != null)
+                    var innerValue = val?.ToString();
+                    if (innerValue is not null)
                     {
-                        var innerValue = val.Value.ToString();
-
-                        //Check if the value contans a comma and place it in quotes if so
+                        //Check if the value contains a comma and place it in quotes if so
                         if (innerValue.Contains(_options.CsvDelimiter))
                             innerValue = string.Concat("\"", innerValue, "\"");
 
                         //Replace any \r or \n special characters from a new line with a space
-                        if (innerValue.Contains("\r"))
-                            innerValue = innerValue.Replace("\r", " ");
-                        if (innerValue.Contains("\n"))
-                            innerValue = innerValue.Replace("\n", " ");
+                        innerValue = innerValue.Replace("\r", " ");
+                        innerValue = innerValue.Replace("\n", " ");
 
                         valueLine = string.Concat(valueLine, innerValue, _options.CsvDelimiter);
                     }
@@ -83,7 +84,7 @@ namespace Be.Vlaanderen.Basisregisters.AspNetCore.Mvc.Formatters.Csv
                     }
                 }
 
-                stringWriter.WriteLine(valueLine.TrimEnd(_options.CsvDelimiter.ToCharArray()));
+                await stringWriter.WriteLineAsync(valueLine.TrimEnd(_options.CsvDelimiter.ToCharArray()));
             }
 
             var streamWriter = new StreamWriter(response.Body);
